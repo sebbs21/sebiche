18797c4a8b2fa92bdf7595cda49cf33aebca0b88
sebbs21 <sebbs21@gmail.com>
2025-03-14 00:26:56 -0500

    prueba

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-// 📌 Archivo: `pages/_app.tsx`
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import "../styles/globals.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
