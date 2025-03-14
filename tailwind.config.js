17b1d6d42994f0da0af2a9ccc8cff8e0c0c3fa62
sebbs21 <sebbs21@gmail.com>
2025-03-13 21:15:47 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#87CEEB",
+        dark: "#2F4F4F",
+        light: "#F8F9FA"
+      }
+    }
+  },
+  plugins: []
+};
