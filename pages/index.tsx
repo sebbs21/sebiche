d09ba95eea8880aa6f476a39a14c1c1b9ff74396
sebbs21 <sebbs21@gmail.com>
2025-03-13 21:16:08 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,25 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import About from "../components/About";
+import Experience from "../components/Experience";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Portfolio de Sebiche</title>
+        <meta name="description" content="Portfolio profesional de Sebiche en UX/UI y tecnología." />
+      </Head>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <About />
+          <Experience />
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+}
 
