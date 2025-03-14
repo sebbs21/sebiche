ad1357cd32e7e84500d2812732a3f51c433c3900
sebbs21 <sebbs21@gmail.com>
2025-03-13 21:17:04 -0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,1 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-dark text-white p-5 text-center">
+      <h1 className="text-4xl font-bold">Sebiche</h1>
+      <p className="text-lg">Diseño, Innovación y Tecnología</p>
+    </header>
+  );
+}
 
