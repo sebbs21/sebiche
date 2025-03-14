a6ea197ebc4a04be26c6424aaae568c200a16a17
sebbs21 <sebbs21@gmail.com>
2025-03-13 21:19:30 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center p-4 mt-10">
+      <p>📩 Contacto: <a href="mailto:soysebiche@gmail.com" className="underline">soysebiche@gmail.com</a></p>
+      <p>© 2025 Sebiche. Todos los derechos reservados.</p>
+    </footer>
+  );
+}
 
